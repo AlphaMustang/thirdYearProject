52f06b07da9a33114c1f0f0aad5adfe95b7dec8d
alphamustang <ryan.noel@hotmail.com>
2019-10-11 14:32:45 +0100

    Checking to see if git works.
    Signed-off-by: alphamustang <ryan.noel@hotmail.com>

--- app/src/main/java/com/example/thirdyearproject/MainActivity.java
+++ app/src/main/java/com/example/thirdyearproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Testing to see if git works
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
